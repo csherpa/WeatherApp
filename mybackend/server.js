3b60f6d9478d9996295f22f222a18cb084ac07aa
chhoti sherpa <chhotisherpa@chhotis-MacBook-Air.local>
2021-01-27 17:28:39 -0600

    packages const assert = require('assert'); const helpers = require('../helper.js');

--- mybackend/server.js
+++ mybackend/server.js
@@ -5,12 +5,13 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send('Weather app running');
-})
+});
 
+adf
 app.get('/weather', (req, res) =>{
     res.send('this is weather endpoint');
     console.log('received a get request');
-})
+});
 
 
 app.listen(port, () =>{
