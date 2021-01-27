d90c1aac1ae01665ddd5c82125b06f062f5f158a
chhoti sherpa <chhotisherpa@chhotis-MacBook-Air.local>
2021-01-27 17:18:58 -0600

    added react-bootstrap and implemented in weather.js

---
+++ myfrontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Weather } from './Weather';
+
+
+export const App = () => {
+    return (
+        <div className="App">
+            {/* <h1>weather page!</h1> */}
+            <Weather/>
+        </div>
+    )
+}
+
---
+++ myfrontend/src/Weather.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+
+export class Weather extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Search Weather</Form.Label>
+                        <Form.Control type="search" placeholder="Enter city" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+                <h1>Weather</h1>
+            </div>
+        )
+    }
+}
