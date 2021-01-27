b86c919ec9c64f7a8d2f0753947d3b70ccc2a479
chhoti sherpa <chhotisherpa@chhotis-MacBook-Air.local>
2021-01-27 15:53:48 -0600

    created a backend folder for the app

---
+++ mybackend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Weather app running');
+})
+
+app.get('/weather', (req, res) =>{
+    res.send('this is weather endpoint');
+    console.log('received a get request');
+})
+
+
+app.listen(port, () =>{
+    console.log('port is running', port)
+});
