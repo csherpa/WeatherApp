903be93f0830a2f10c7ef3a8328b9202615cad02
chhoti sherpa <chhotisherpa@chhotis-MacBook-Air.local>
2021-01-27 20:27:10 -0600

    fixed gitignore file

--- mybackend/server.js
+++ mybackend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 
 const port = process.env.PORT || 3000
@@ -7,11 +8,11 @@ app.get('/', (req, res) => {
     res.send('Weather app running');
 });
 
-adf
 app.get('/weather', (req, res) =>{
     res.send('this is weather endpoint');
     console.log('received a get request');
 });
+
 
 
 app.listen(port, () =>{
