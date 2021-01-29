adf8a4248bd61723391dfe78dbf149348cd8cf40
chhoti sherpa <chhotisherpa@chhotis-MacBook-Air.local>
2021-01-29 15:46:06 -0600

    added node-fetch and fixed server.js

--- mybackend/server.js
+++ mybackend/server.js
@@ -1,44 +1,46 @@
 const express = require('express');
-const request = require('request');
-const app = express();
+const fetch = require('node-fetch');
 require('dotenv').config();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-const port = process.env.PORT || 3000
-
-//const cityName = process.argv[2]
-
-const URL = `http://api.openweathermap.org/data/2.5/weather?q=dallas&appid=${process.env.API_KEY}`
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Weather app ');
-    console.log('server.js is working');
+    const date = new Date();
+    res.json({currentTime: date.toTimeString() });
+    console.log('got the get request');
 });
 
-app.get('/weather', (req, res) =>{
-    res.send('this is weather endpoint');
-});
-
-app.get('/cityWeather', (req, res) =>{
-    request(URL, (err, response, body) => {
-        if(!err && response.statusCode == 200){
-            const data = JSON.parse(body);
-            console.log(data.main);
-            res.send(data.main);
-        }
-   });
-
-})
-
-// request(URL, (error, response, body) =>{
-//     const data = JSON.parse(body);
-//     console.log(data.main);
-// })
+//get weather by cityname
+app.get('/:cityName', (req, res) => {
+    const cityName = req.params.cityName;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.API_KEY}`
     
+    return fetch(url)
+        .then((response) => response.json())
+        .then((data) => res.send(data))
+        .catch((err) => console.log(err));
+
+});
+
+
+
 app.listen(port);
 
-// app.listen(port, () =>{
-//     console.log('port is running', port)
+// app.get('/api/geo/:citySearch', (req, res) => {
+//     const searchUrl = `http://gd.geobytes.com/AutoCompleteCity?q=${req.params.city}`
+//     return fetch(searchUrl)
+//         .then((data) => data.json())
+//         .then((data) => console.log(data))
+//         .catch((err) => console.log(err));      
 // });
+    
+// const weather = {
+        //     city: city,
+        //     temperature: Math.round((cityWeather.main.temp - 273.15) * 9/5 + 32),
+        //     temp_max: Math.round((cityWeather.main.temp_max - 273.15) * 9/5 + 32),
+        //     temp_min: Math.round((cityWeather.main.temp_min - 273.15) * 9/5 + 32),
+        //     humidity: cityWeather.main.humidity,
+        //     description: cityWeather.weather[0].description,
+        //     windspeed: cityWeather.wind.speed
+        // }
