9b4ca73f2e0d7297fa623dcb6808ff77168b12d8
chhoti sherpa <chhotisherpa@chhotis-MacBook-Air.local>
2021-01-30 14:12:36 -0600

    created some files in components

--- myfrontend/src/App.js
+++ myfrontend/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Weather } from './Weather';
+import { Weather } from './components/Weather';
+import { Search } from './components/Search';
+import { DefaultWeather } from './components/DefaultWeather';
 
 
 
-export  class App extends React.Component{
-    render(){
-        return (
-            <div>
-                <Weather/>
-            </div>
-        )
-    }    
+export const App = () =>{
+
+    return (
+        <div>
+            <Search/>
+            <Weather/>
+            
+        </div>
+    ) 
     
 }
 
---
+++ myfrontend/src/components/Weather.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export class Weather extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div>
+            </div>
+        )
+    }
+}
