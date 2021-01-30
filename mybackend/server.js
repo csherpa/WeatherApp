2df64d926da3b49f54f209dc1ef574502ed85f83
chhoti sherpa <chhotisherpa@chhotis-MacBook-Air.local>
2021-01-30 14:48:49 -0600

    fixed the api call to fetch city's current/hourly/daily weather

--- mybackend/server.js
+++ mybackend/server.js
@@ -14,22 +14,31 @@ app.get('/', (req, res) => {
     console.log('got the get request');
 });
 
+app.get('/api/:cityName', (req, res) =>{
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${req.params.cityName}&appid=${process.env.API_KEY}`
 
-
-app.get('/api/:latitude/:longitude', (req, res) => {
-    //const url = `http://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${process.env.API_KEY}`
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${req.params.latitude}&lon=${req.params.longitude}&units=metric&exclude=minutely&appid=${process.env.API_KEY}`
-    return fetch(url)
+    fetch(url)
         .then((response) => response.json())
-        .then((data) => res.send(data))
+        .then((data) => {        
+            const lat = data.coord.lat;
+            const lon = data.coord.lon;
+
+            const url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely&appid=${process.env.API_KEY}`
+            fetch(url2)
+                .then((res) => res.json())
+                .then((data) => { 
+                    res.send(data)
+                })
+                .catch((err) => console.log(err));
+        })
         .catch((err) => console.log(err));
-});
+})
 
 app.get('/api/geo/:city', (req, res) => {
     return fetch(`http://gd.geobytes.com/AutoCompleteCity?q=${req.params.city}`)
         .then(cities => cities.json())
         .then((cities) => res.json(cities))
-        .catch((error) => console.log('error', error));
+        .catch((error) => console.log(error));
 }
 );
 
@@ -37,7 +46,7 @@ app.get('/api/details/:city', (req, res) => {
     return fetch(`http://gd.geobytes.com/GetCityDetails?fqcn=${req.params.city}`)
         .then(details => details.json())
         .then((details) => res.json(details))
-        .catch((error) => console.log('error', error));
+        .catch((error) => console.log(error));
 }
 );
 
