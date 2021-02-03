a46c28149d63596a3b947d17afe1fb0800302097
chhoti sherpa <chhotisherpa@chhotis-MacBook-Air.local>
2021-02-03 17:08:21 -0600

    deleted myfrontend/mybackend folder , initialized new package.json and created new folder

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000
+
+const app = express();
+app.use(express.static(__dirname + "/public"));
+
+app.get('/',(req,res)=>{
+    res.send('server working')
+})
+app.get('/home', (req,res) =>{
+    res.sendFile(path.resolve(__dirname + "/public/index.html"))
+})
+
+app.listen(PORT);
