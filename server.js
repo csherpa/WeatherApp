ccf14965668255ba11812b169b9a7170be90a851
chhoti sherpa <chhotisherpa@chhotis-MacBook-Air.local>
2021-02-03 17:39:48 -0600

    add weather api on the server and installed packages to fetch the api

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const fetch = require('node-fetch');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5000
 
@@ -13,5 +15,30 @@ app.get('/home', (req,res) =>{
     console.log('working')
     res.sendFile(path.resolve(__dirname + "/public/index.html"))
 })
+
+app.get('/api/:city', (req, res) =>{
+    const cityName = req.params.city;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.API_KEY}`
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {     
+            console.log(data)   
+            const Lat = data.coord.lat;
+            //console.log(Lat)
+            const Lon = data.coord.lon;
+            //console.log(Lon)
+
+            const url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${Lat}&lon=${Lon}&units=metric&exclude=minutely&appid=${process.env.API_KEY}`
+            fetch(url2)
+                .then((res) => res.json())
+                .then((data) => { 
+                    res.send(data)
+                })
+                .catch((err) => console.log(err));
+        })
+        .catch((err) => console.log(err));
+})
+
 
 app.listen(PORT);
