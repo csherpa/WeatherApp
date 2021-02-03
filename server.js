09b29738704b80a20215076fe0dc1456ba2106b6
chhoti sherpa <chhotisherpa@chhotis-MacBook-Air.local>
2021-02-03 17:20:26 -0600

    install new packages for react

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const path = require('path');
 const PORT = process.env.PORT || 5000
 
 const app = express();
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
 
 app.get('/',(req,res)=>{
-    res.send('server working')
+    res.send({working: true})
 })
 app.get('/home', (req,res) =>{
+    console.log('working')
     res.sendFile(path.resolve(__dirname + "/public/index.html"))
 })
 
